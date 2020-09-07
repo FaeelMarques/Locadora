48875331aa600904de24318cb620394460c52fe5
Rafael Marques Taranto <faeel.marques@gmail.com>
2020-09-07 05:21:27 -0300

    Adicionado mapeamento com automapper

--- LocadoraDDD/LocadoraDDD.MVC/Global.asax.cs
+++ LocadoraDDD/LocadoraDDD.MVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraDDD.MVC.AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,9 @@ namespace LocadoraDDD.MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            AutoMapperConfig.RegisterMappings();
         }
     }
 }
---
+++ LocadoraDDD/LocadoraDDD.MVC/Models/FilmeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LocadoraDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraDDD.MVC.Models
+{
+    public class FilmeViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public virtual Genero Genero { get; set; }
+    }
+}
