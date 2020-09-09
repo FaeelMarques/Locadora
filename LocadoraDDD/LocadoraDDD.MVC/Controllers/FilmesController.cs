f33a6a13d1e895bb0a01891d05e5897049bd8845
Rafael Marques Taranto <faeel.marques@gmail.com>
2020-09-09 11:44:11 -0300

    descomentado linha authorize da controller de filmes

--- LocadoraDDD/LocadoraDDD.MVC/Controllers/FilmesController.cs
+++ LocadoraDDD/LocadoraDDD.MVC/Controllers/FilmesController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace LocadoraDDD.MVC.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class FilmesController : Controller
     {
         private readonly IGeneroAppService _generoService;
